732bfd20a05fea50fac95dbd6110e8cb4d1fefb6
sruth <sruthi.soorian@gmail.com>
2022-12-14 01:23:50 -0500

    Finished Most settings

--- app/src/main/java/com/example/bofadigitalbusinesscard/MainActivity.java
+++ app/src/main/java/com/example/bofadigitalbusinesscard/MainActivity.java
@@ -11,8 +11,15 @@ import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    private static boolean STARTUP = true;
+
     ImageButton settingsButton;
     ImageView qrCode;
+    ImageView headshotImage, bofaLogo;
+    TextView jobTag, nameTag, companyTag, cellTag, emailTag, cardDecal;
+
+    String name, company, job, cell, email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +29,43 @@ public class MainActivity extends AppCompatActivity {
         //Set Components to variables
         settingsButton = (ImageButton) findViewById(R.id.settingsButton);
         qrCode = findViewById(R.id.QrCode);
+        headshotImage = findViewById(R.id.headshotImage);
+        jobTag = findViewById(R.id.jobTag);
+        nameTag = findViewById(R.id.nameTag);
+        companyTag = findViewById(R.id.companyTag);
+        cellTag = findViewById(R.id.cellTag);
+        emailTag = findViewById(R.id.emailTag);
+        bofaLogo = findViewById(R.id.bofaLogo);
+        cardDecal = findViewById(R.id.decal);
+
+        //Declare string values
+        if (STARTUP == true){
+            name = "Sruthi Soorian";
+            job = "Software Engineer (TAP 2022)";
+            company = "GBT-GCIB AND SALES TECHNOLOGY";
+            cell = "555-123-4567";
+            email = "sruthi.soorian@bofa.com";
+            ranOnce();
+        }
+        else{
+            Intent intent = getIntent();
+            name = intent.getExtras().getString("name");
+            job = intent.getExtras().getString("job");
+            company = intent.getExtras().getString("company");
+            cell = intent.getExtras().getString("cell");
+            email = intent.getExtras().getString("email");
+
+        }
+
+        //Set default strings
+        nameTag.setText(name);
+        jobTag.setText(job);
+        companyTag.setText(company);
+        cellTag.setText(cell);
+        emailTag.setText(email);
 
         //VCard Dissection
-        String vCardString = "BEGIN:VCARD\n" +
-                "N:Sruthi\n" +
-                "FN: Sruthi Soorian\n" +
-                "ORG: GBT-GCIB And Sales Technology\n" +
-                "TITLE: Software Engineer (TAP 2022)\n" +
-                "TEL;TYPE#WORK,VOICE:(111) 555-1212\n" +
-                "EMAIL: sruthi.soorian@bofa.com\n" +
-                "END:VCARD";
+
 
 
 
@@ -48,6 +82,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void openSettingsPage(){
         Intent intent = new Intent(this, SettingsActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("job", job);
+        intent.putExtra("company", company);
+        intent.putExtra("cell", cell);
+        intent.putExtra("email", email);
         startActivity(intent);
+    }
+
+    private void ranOnce(){
+        if(STARTUP){
+            STARTUP = false;
+        }
     }
 }
--- app/src/main/java/com/example/bofadigitalbusinesscard/SettingsActivity.java
+++ app/src/main/java/com/example/bofadigitalbusinesscard/SettingsActivity.java
@@ -9,8 +9,16 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.*;
 
+import java.util.ArrayList;
+
 public class SettingsActivity extends AppCompatActivity {
     ImageButton exitButton;
+    EditText editCompany, editName, editPhone, editPosition, editEmail;
+
+    private Spinner editTheme;
+    ArrayList<String> spinnerArray = new ArrayList<String>();
+
+    String name, company, job, cell, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,40 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         //Set Components to variables
-        exitButton = (ImageButton) findViewById(R.id.exitButton);
+        exitButton = findViewById(R.id.exitButton);
+        editCompany = findViewById(R.id.editCompany);
+        editName = findViewById(R.id.editName);
+        editPhone = findViewById(R.id.editPhone);
+        editPosition = findViewById(R.id.editPosition);
+        editEmail = findViewById(R.id.editEmail);
+        editTheme = findViewById(R.id.editTheme);
+
+        //Set the text in the field
+        Intent intent = getIntent();
+        name = intent.getExtras().getString("name");
+        job = intent.getExtras().getString("job");
+        company = intent.getExtras().getString("company");
+        cell = intent.getExtras().getString("cell");
+        email = intent.getExtras().getString("email");
+
+        editName.setText(name);
+        editPosition.setText(job);
+        editCompany.setText(company);
+        editPhone.setText(cell);
+        editEmail.setText(email);
+
+        //Populate Spinner
+        spinnerArray.add("Default Grey");
+        spinnerArray.add("Business Blue");
+        spinnerArray.add("Profit Pink");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        editTheme.setAdapter(adapter);
+
+
 
         //Exit Settings
         exitButton.setOnClickListener(new View.OnClickListener(){
@@ -29,8 +70,20 @@ public class SettingsActivity extends AppCompatActivity {
         });
     }
 
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getApplicationContext(), choice, Toast.LENGTH_LONG).show();
+    }
+
     public void closeSettingsPage(){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name", editName.getText().toString());
+        intent.putExtra("job", editPosition.getText().toString());
+        intent.putExtra("company", editCompany.getText().toString());
+        intent.putExtra("cell", editPhone.getText().toString());
+        intent.putExtra("email", editEmail.getText().toString());
         startActivity(intent);
     }
+
+
 }
